de1811204908ebb0c211626aa4d66ea7750a727d
MATTHEW RUTLAND <A0072461@live.tees.ac.uk>
2023-01-07 14:03:04 +0000

    altered connection string + began adding async methods

---
+++ BasicProductsApi/ProductContext.cs
@@ -0,0 +1,14 @@
+﻿global using Microsoft.EntityFrameworkCore;
+
+namespace BasicProductsApi
+{
+    public class ProductContext : DbContext
+    {
+        public ProductContext(DbContextOptions<ProductContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Product> Products => Set<Product>();
+    }
+}
--- BasicProductsApi/Program.cs
+++ BasicProductsApi/Program.cs
@@ -1,9 +1,14 @@
+using BasicProductsApi;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<ProductContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
+);
 
 var app = builder.Build();
 
@@ -17,5 +22,7 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.MapGet("/", () => "This is a test message");
+
+app.MapGet("/products", async (ProductContext context) => await context.Products.ToListAsync());
 
 app.Run();
