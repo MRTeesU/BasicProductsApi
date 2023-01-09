441549e52c252bbf32598ce7b3e885a5c3ab781f
MATTHEW RUTLAND <A0072461@live.tees.ac.uk>
2023-01-09 19:19:25 +0000

    a

--- BasicProductsApi/Program.cs
+++ BasicProductsApi/Program.cs
@@ -33,7 +33,7 @@ app.UseHttpsRedirection();
 
 async Task<List<Product>> GetAllProducts(ProductContext context) => await context.Products.ToListAsync();
 
-app.MapGet("/", () => "This is a test mmessage");
+app.MapGet("/", () => "This is a test mmessage a");
 
 app.MapGet("/products", async (ProductContext context) => await context.Products.ToListAsync());
 
